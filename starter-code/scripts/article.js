e37ff492d1725918191f1ace7ad03945990cab14
Isaiah Walker <Walkisai.72@gmail.com>
2017-07-14 11:17:59 -0700

    Isaiah(driver)-Brendan(nav) We completed the last two TODO's in the articles.js which involved using the template to create the newSection and appending it to the DOM. This one took roughly 1hour and 45mins.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,9 +12,17 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // TODONE: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
+  var template = $('#template').html();
+  console.log(template);
+  var compile = Handlebars.compile(template);
+
+  // $(articles).each(function() {
+  var newSection = compile(this);
+  console.log(newSection);
+  // });
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
@@ -23,8 +31,9 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+  // TODONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  // $('#articles').append(newSection);
+  return newSection;
 };
 
 rawData.sort(function(a,b) {
